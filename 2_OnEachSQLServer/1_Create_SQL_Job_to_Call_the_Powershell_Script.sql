2275a082d59dd0f982fe81cb9bc241c53acdad47
George Walkey <gwalkey@users.noreply.github.com>
2019-10-21 19:43:07 -0400

    Add files via upload

---
+++ 2_OnEachSQLServer/1_Create_SQL_Job_to_Call_the_Powershell_Script.sql
@@ -0,0 +1,53 @@
+USE [msdb]
+GO
+
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'_SQL_Alerts_Call_Powershell', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'Operator', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Send to Centralized SQL Table', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'CmdExec', 
+		@command=N'powershell.exe c:\psscripts\logsqlalert.ps1 ''$(ESCAPE_SQUOTE(A-SVR))'' ''$(ESCAPE_SQUOTE(A-DBN))'' ''$(ESCAPE_SQUOTE(A-ERR))'' ''$(ESCAPE_SQUOTE(A-SEV))'' ''$(ESCAPE_SQUOTE(A-MSG))''', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
---
+++ 2_OnEachSQLServer/logsqlalert.ps1
@@ -0,0 +1,56 @@
+﻿[CmdletBinding()]
+Param(
+    [string]$ServerName,
+    [string]$DatabaseName,
+    [string]$ErrorNumber,
+    [string]$ErrorSeverity,
+    [string]$ErrorMessage
+)
+
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\Connect_SQLServer_Module.psm1"
+
+# This is the name of your SQL Server that hosts the Centralized Database [SQLAlerts]
+$DatabaseServer='sqlprod01'
+
+$SQLErrMsg = $ErrorMessage.Replace([string]([char]39),'')
+
+$sqlcmd1=
+"
+INSERT INTO dbo.AgentAlerts
+(
+    [Server],
+    [Database],
+    [EventTime],
+    [ErrorNumber],
+    [ErrorSeverity],
+    [ErrorMessage]
+)
+VALUES
+(   '$ServerName',
+    '$DatabaseName',
+    SYSDATETIME(),
+    '$ErrorNumber',
+    '$ErrorSeverity',
+    '$SQLErrMsg'
+    )
+"
+$sqlcmd1
+
+try
+{
+    $sqlresults = Connect-InternalSQLServer -SQLInstance $DatabaseServer -Database "SQLAlerts" -SQLExec $sqlCMD1 -ErrorAction stop
+}
+catch
+{
+    $mailParams=@{
+        To = "DBA@Company.com"
+        From = "powershell_scripting@company.com"
+        Subject = "Error doing INSERT into [SQLAlerts].[dbo].[AgentAlerts] on [$DatabaseServer]"
+        SMTPServer = "mail.yourdomain.com"
+        Body = $Error[0]
+        BodyAsHTML = $True
+    }
+
+    send-mailmessage @mailparams
+}
+
