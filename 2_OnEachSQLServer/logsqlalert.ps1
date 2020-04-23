738a716bd193293ffe0c3a6ba8228ea0629bf412
George Walkey <gwalkey@users.noreply.github.com>
2020-04-23 17:13:26 -0400

    Add files via upload

--- 2_OnEachSQLServer/logsqlalert.ps1
+++ 2_OnEachSQLServer/logsqlalert.ps1
@@ -1,4 +1,26 @@
-﻿[CmdletBinding()]
+﻿<#
+.SYNOPSIS
+	Sends SQL Agent Alert Tokens and INSERTs into a SQL Database
+.DESCRIPTION
+	
+.EXAMPLE
+ 	
+.EXAMPLE
+ 
+.EXAMPLE
+ 
+.Inputs
+   
+.Outputs
+
+.NOTES
+   
+.LINK
+    https://github.com/gwalkey/SQL-Agent-Repo
+
+#>
+
+[CmdletBinding()]
 Param(
     [string]$ServerName,
     [string]$DatabaseName,
@@ -7,7 +29,46 @@ Param(
     [string]$ErrorMessage
 )
 
-Import-Module "C:\Program Files\WindowsPowerShell\Modules\Connect_SQLServer_Module.psm1"
+Function Connect-InternalSQLServer
+{   
+    [CmdletBinding()]
+    Param([String]$SQLExec,
+          [String]$SQLInstance,
+          [String]$Database)
+
+    Process
+    {
+        # Open connection and Execute sql against server using Windows Auth
+        $DataSet = New-Object System.Data.DataSet
+        $SQLConnectionString = "Data Source=$SQLInstance;Initial Catalog=$Database;Integrated Security=SSPI;" 
+        $Connection = New-Object System.Data.SqlClient.SqlConnection
+        $Connection.ConnectionString = $SQLConnectionString
+        $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+        $SqlCmd.CommandText = $SQLExec
+        $SqlCmd.Connection = $Connection
+        $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+        $SqlAdapter.SelectCommand = $SqlCmd
+        $SqlCmd.CommandTimeout=60
+   
+        # Insert results into Dataset table
+        $SqlAdapter.Fill($DataSet) | out-null
+        # Eval Return Set
+        if ($DataSet.Tables.Count -ne 0) 
+        {
+            $sqlresults = $DataSet.Tables[0]
+        }
+        else
+        {
+            $sqlresults =$null
+        }
+
+        # Close connection to sql server
+        $Connection.Close()
+
+        Write-Output $sqlresults
+    }
+}
+
 
 # This is the name of your SQL Server that hosts the Centralized Database [SQLAlerts]
 $DatabaseServer='sqlprod01'
